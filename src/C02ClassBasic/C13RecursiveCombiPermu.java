4888b935890818de5bcd2531926957d889fc93a4
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-20 15:01:32 +0900

    Auto commit by publicguard_

--- src/C02ClassBasic/C13RecursiveCombiPermu.java
+++ src/C02ClassBasic/C13RecursiveCombiPermu.java
@@ -3,6 +3,6 @@ package C02ClassBasic;
 // 조합(combination)과 순열(permutation) -> 이런 유형의 문제는 왠만하면 재귀로
 public class C13RecursiveCombiPermu {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("안녕");
     }
 }
