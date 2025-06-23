55ee3dfc1ed2ae99ca052ee1088dd1ee551e7500
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 14:26:43 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankService.java
+++ src/C04Interface/BankService/BankService.java
@@ -2,7 +2,8 @@ package C04Interface.BankService;
 
 public class BankService {
     void deposit(long money, BankAccount ba){
-
+//    현재 얼마있는지 조회
+//    ㅎ
     }
 
     void withdraw(long money, BankAccount ba){
