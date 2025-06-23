576b17c5e7b151419b489bfbf1955d04d3768ca1
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 14:05:09 +0900

    자동 커밋됨

--- src/C04Interface/C01InterfaceMain.java
+++ src/C04Interface/C01InterfaceMain.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class C04InterfaceMain {
+public class C01InterfaceMain {
     public static void main(String[] args) {
         C01Cat c1 = new C01Cat();
         c1.makeSound();
