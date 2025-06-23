fe0141d807308d82e46417f7632f91214d5dc173
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:41:13 +0900

    자동 커밋됨

--- src/C03Inheritance/C0302SuperKeyword.java
+++ src/C03Inheritance/C0302SuperKeyword.java
@@ -6,6 +6,10 @@ public class C0302SuperKeyword {
         C0302SuperKeyword c2 = new C0302SuperKeyword();
         System.out.println(c2.a);
     }
+
+    public void display(){
+        
+    }
 }
 
 class SuperParents{
