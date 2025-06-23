81e128222934ebb96a9b3eefa5101b9d019e4460
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:47:36 +0900

    자동 커밋됨

--- src/C03Inheritance/C0302SuperKeyword.java
+++ src/C03Inheritance/C0302SuperKeyword.java
@@ -3,6 +3,8 @@ package C03Inheritance;
 // 부모클래스를 상속받을때, 부모클래스에 기본생성자가 없으면 자식클래스에서 상속시 기본적으로 에러발생.
 public class C0302SuperKeyword extends SuperParents{
     int a = 10;
+
+    C0302SuperKeyword
     public static void main(String[] args) {
         C0302SuperKeyword c2 = new C0302SuperKeyword();
         System.out.println(c2.a);
