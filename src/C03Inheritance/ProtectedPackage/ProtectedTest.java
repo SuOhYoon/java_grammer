d1fdd5e101c5b3e6a2f4224840e1ecc5896c95ee
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:44:56 +0900

    자동 커밋됨

--- src/C03Inheritance/ProtectedPackage/ProtectedTest.java
+++ src/C03Inheritance/ProtectedPackage/ProtectedTest.java
@@ -10,7 +10,7 @@ public class ProtectedTest {
 //        public접근제어자는 프로젝트 전역에서 접근가능
         p1.st1 = "hello python";
 //        패키지가 달라졌으므로 defualt변수는 접근불가
-        p1.st3 = "hello python";
+//        p1.st3 = "hello python";
 //        상속관계가 있을경우, 패키지가 달라도 protected변수에 접근 가능.
         p1.st4 = "hello python";
     }
