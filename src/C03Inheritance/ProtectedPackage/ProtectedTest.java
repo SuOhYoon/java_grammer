8654cf97102e349949741c59f2c7e6737fde307f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:47:04 +0900

    자동 커밋됨

--- src/C03Inheritance/ProtectedPackage/ProtectedTest.java
+++ src/C03Inheritance/ProtectedPackage/ProtectedTest.java
@@ -4,6 +4,7 @@ package C03Inheritance.ProtectedPackage;
 // 다만, 패키지내의 패키지안의 class는 *로 import불가
 import C03Inheritance.C04ProtectedClass;
 
+// class에는 public, default 접근 제어자만 존재
 public class ProtectedTest extends C04ProtectedClass {
     public static void main(String[] args) {
         ProtectedTest p1 = new ProtectedTest();
