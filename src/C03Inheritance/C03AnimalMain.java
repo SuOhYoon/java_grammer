de2a7f1b427d164e87665f2f4d9ede9880b42a6a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:09:33 +0900

    자동 커밋됨

--- src/C03Inheritance/C03AnimalMain.java
+++ src/C03Inheritance/C03AnimalMain.java
@@ -2,7 +2,10 @@ package C03Inheritance;
 
 public class C03AnimalMain {
     public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.sound();
 
+        Cat c1 = 
     }
 }
 
