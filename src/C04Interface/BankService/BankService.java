b19af99f92eb70316ffd97b14940ad49500a8678
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 15:16:06 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankService.java
+++ src/C04Interface/BankService/BankService.java
@@ -4,8 +4,9 @@ public class BankService {
     void deposit(long money, BankAccount ba){
 //        현재 얼마있는지 조회
         long current = ba.getBalance();
-//        그 금액과 money와 합산하여 updateBalance
         long newBalance = current + money;
+//        그 금액과 money와 합산하여 updateBalance
+        ba.updateBalance(newBalance);
     }
 
     boolean withdraw(long money, BankAccount ba){
