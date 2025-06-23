9c15602ddb407414c995a76921c4cdbcbfdbb8a1
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 15:25:52 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankController.java
+++ src/C04Interface/BankService/BankController.java
@@ -18,7 +18,7 @@ public class BankController {
                 System.out.println("입금방식을 선택해주세요. 1.카카오페이 2.토스페이");
                 System.out.println("입금하실 금액을 입력해주세요");
                 long money = Long.parseLong(sc.nextLine());
-                bs.deposit(money, ba);
+                if()
             } else {
                 System.out.println("입금방식을 선택해주세요. 1.카카오페이 2.토스페이");
                 System.out.println("출금하실 금액을 입력해주세요");
