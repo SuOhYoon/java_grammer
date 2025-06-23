28828114ecb1b6cf69b6e852bcbaecd28232704a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 12:12:16 +0900

    자동 커밋됨

--- src/C04Interface/C04InterfaceMain.java
+++ src/C04Interface/C04InterfaceMain.java
@@ -8,7 +8,7 @@ public class C04InterfaceMain {
         C01Dog d1 = new C01Dog();
         d1.makeSound();
 
-        C01AnimalInterface1 c2 = new C01Cat();
+        C04AnimalInterface1 c2 = new C01Cat();
         c1.makeSound();
 
         C01Dog d2 = new C01Dog();
