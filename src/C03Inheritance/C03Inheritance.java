3d4f8e974996dca37f94cd044c1de17978e9c018
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:08:49 +0900

    자동 커밋됨

--- src/C03Inheritance/C03Inheritance.java
+++ src/C03Inheritance/C03Inheritance.java
@@ -2,6 +2,11 @@ package C03Inheritance;
 
 public class C03Inheritance {
     public static void main(String[] args) {
-        
+
     }
+}
+
+class Parents {
+    int a = 20;
+    private int b = 20;
 }
