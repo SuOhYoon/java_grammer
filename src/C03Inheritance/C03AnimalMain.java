ae766e1baa0bb76b3cf20eee4d01180f06d9fdab
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:13:43 +0900

    자동 커밋됨

--- src/C03Inheritance/C03AnimalMain.java
+++ src/C03Inheritance/C03AnimalMain.java
@@ -9,6 +9,7 @@ public class C03AnimalMain {
         c1.sound();
 
 //        상속 관계 일떄는 부모클래스의 타입을 자식클래스의 객체의 타입으로 지정하는 것이 가능
+
         Animal d2 = new Dog();
         d2.sound();
 //        d2.sound2();
