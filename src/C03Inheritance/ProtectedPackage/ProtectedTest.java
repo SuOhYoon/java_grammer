d511431144eb227627a0161e7d94435f51f78b3e
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:31:25 +0900

    자동 커밋됨

--- src/C03Inheritance/ProtectedPackage/ProtectedTest.java
+++ src/C03Inheritance/ProtectedPackage/ProtectedTest.java
@@ -5,5 +5,6 @@ import C03Inheritance.C04ProtectedClass;
 public class ProtectedTest {
     public static void main(String[] args) {
         C04ProtectedClass p1 = new C04ProtectedClass();
+        p1.st1 = "hello python"
     }
 }
