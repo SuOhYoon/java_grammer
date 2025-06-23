0351fb66ada87985a374a9d14d3aaf62e74c8f3f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:31:11 +0900

    자동 커밋됨

--- src/C03Inheritance/ProtectedPackage/ProtectedTest.java
+++ src/C03Inheritance/ProtectedPackage/ProtectedTest.java
@@ -1,4 +1,9 @@
 package C03Inheritance.ProtectedPackage;
 
+import C03Inheritance.C04ProtectedClass;
+
 public class ProtectedTest {
+    public static void main(String[] args) {
+        C04ProtectedClass p1 = new C04ProtectedClass();
+    }
 }
