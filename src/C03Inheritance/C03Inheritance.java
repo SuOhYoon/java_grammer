41b6cd20ad315b8b813526df56261188e594eaaa
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:25:21 +0900

    자동 커밋됨

--- src/C03Inheritance/C03Inheritance.java
+++ src/C03Inheritance/C03Inheritance.java
@@ -14,6 +14,7 @@ public class C03Inheritance extends Parents {
         c1.m2();
     }
 
+//    부모 메서드와 재정의(overriding) : 부모 클래스의 메서드명과 자식클래스의 메서드명이 동일한 상황
     public void m2(){
         System.out.println("자식 클래스의 m2메서드 입니다.");
     }
