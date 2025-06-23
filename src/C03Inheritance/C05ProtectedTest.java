6f1f10bb96c3f5946bdbfaf3a4cbfe4c8c5a7677
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:25:36 +0900

    자동 커밋됨

--- src/C03Inheritance/C05ProtectedTest.java
+++ src/C03Inheritance/C05ProtectedTest.java
@@ -3,7 +3,7 @@ package C03Inheritance;
 public class C05ProtectedTest {
     public static void main(String[] args) {
         C04ProtectedClass p1 = new C04ProtectedClass();
-        C04ProtectedClass.st1 = "hello python";
+        p1.st1 = "hello python";
 //        C04ProtectedClass.st2 = "hello python";
     }
 }
