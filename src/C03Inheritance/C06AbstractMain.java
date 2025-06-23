74b0578968c16ee4317435adbe78deaef9de82cc
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 11:19:03 +0900

    자동 커밋됨

--- src/C03Inheritance/C06AbstractMain.java
+++ src/C03Inheritance/C06AbstractMain.java
@@ -10,6 +10,9 @@ class AbstractAnimal{
     void makeSound(){
         System.out.println("동물은 소리를 냅니다.");
     }
+
+//    메서드를 선언만하고 구현이 없는 메서드 선언시 abstract키워드 사용.
+    abstract void makesound2();
 }
 
 class AbstractDog{
