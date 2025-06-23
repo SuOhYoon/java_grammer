2e662367554a7860dc9059404ceca2fe04b4677d
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 15:31:48 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankController.java
+++ src/C04Interface/BankService/BankController.java
@@ -12,24 +12,24 @@ public class BankController {
         while(true){
             System.out.println("입금하시려면 1번, 출금하시려면 2번");
             System.out.println("입금방식을 선택해주세요. 1.카카오페이 2.토스페이");
-            
+
             String serviceNumber = sc.nextLine();
             BankService bs;
-            
+            if(bank.equals("1")){
+                bs = new BankKakaoService();
+                bs.deposit(money, ba);
+                System.out.println("입금이 완료되었습니다.");
+            } else {
+                bs = new BankTossService();
+                bs.deposit(money, ba);
+                System.out.println("입금이 완료되었습니다.");
+            }
             if(serviceNumber.equals("1")){
 
                 System.out.println("입금하실 금액을 입력해주세요");
                 long money = Long.parseLong(sc.nextLine());
                 String bank = sc.nextLine();
-                if(bank.equals("1")){
-                    bs = new BankKakaoService();
-                    bs.deposit(money, ba);
-                    System.out.println("입금이 완료되었습니다.");
-                } else {
-                    bs = new BankTossService();
-                    bs.deposit(money, ba);
-                    System.out.println("입금이 완료되었습니다.");
-                }
+
             } else {
                 System.out.println("입금방식을 선택해주세요. 1.카카오페이 2.토스페이");
                 System.out.println("출금하실 금액을 입력해주세요");
