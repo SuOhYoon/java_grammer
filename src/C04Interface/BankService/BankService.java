baf39747db537b6eeba735ba10156ffa9f587e17
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 15:16:26 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankService.java
+++ src/C04Interface/BankService/BankService.java
@@ -11,7 +11,9 @@ public class BankService {
 
     boolean withdraw(long money, BankAccount ba){
 //        현재 얼마있는지 조회 && 출금금액과 비교
-        
+        if(current < money){
+            
+        }
 //        그 금액과 money와 차감하여 updateBalance
         return false;
     }
