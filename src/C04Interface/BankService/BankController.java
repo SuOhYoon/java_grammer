bfeaf8df3bb99518be5d09088b2571f265ebc76a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 14:20:44 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankController.java
+++ src/C04Interface/BankService/BankController.java
@@ -6,6 +6,6 @@ public class BankController {
     public static void main(String[] args) {
         System.out.println("계좌번호를 입력하세요");
         Scanner sc = new Scanner(System.in);
-        String accountNumber = sc.nextInt();
+        String accountNumber = sc.nextLine();
     }
 }
