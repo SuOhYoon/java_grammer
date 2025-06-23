9c34f4869e768b624e37b9c339c55dcb299633b5
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:14:32 +0900

    자동 커밋됨

--- src/C03Inheritance/C03Inheritance.java
+++ src/C03Inheritance/C03Inheritance.java
@@ -3,7 +3,7 @@ package C03Inheritance;
 // extends 키워드를 통해 상속을 수행 -> 부모의 변수와 메서드를 자식클래스에서 물려받는것.
 public class C03Inheritance extends Parents {
     public static void main(String[] args) {
-        C0301
+        C03Inheritance
     }
 }
 
