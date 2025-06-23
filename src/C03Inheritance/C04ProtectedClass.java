9bf430c850e67c01eb2a5c137aa81ec46927d2fa
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 10:21:54 +0900

    자동 커밋됨

--- src/C03Inheritance/C04ProtectedClass.java
+++ src/C03Inheritance/C04ProtectedClass.java
@@ -4,5 +4,5 @@ public class C04ProtectedClass {
 
 //        변수, 메서드에는 4가지 접근제어자가 존재
 //        public : 프로젝트 전체에서 접근가능(다른패키지여도 접근가능)
-    public String st1 = "hello java1";
+    static public String st1 = "hello java1";
 }
