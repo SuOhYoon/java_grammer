979f0f1a0f8fe18592fd434b28295a8463138c4a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 15:26:54 +0900

    자동 커밋됨

--- src/C04Interface/BankService/BankController.java
+++ src/C04Interface/BankService/BankController.java
@@ -19,7 +19,9 @@ public class BankController {
                 System.out.println("입금하실 금액을 입력해주세요");
                 long money = Long.parseLong(sc.nextLine());
                 String bank = sc.nextLine();
-                if(bank.equals)
+                if(bank.equals("1")){
+                    bs = BankKakaoService
+                }
             } else {
                 System.out.println("입금방식을 선택해주세요. 1.카카오페이 2.토스페이");
                 System.out.println("출금하실 금액을 입력해주세요");
