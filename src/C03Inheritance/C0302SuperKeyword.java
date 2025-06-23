7dfa2e0b12881a7b0a166bf406cb94eb2027b413
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:43:49 +0900

    자동 커밋됨

--- src/C03Inheritance/C0302SuperKeyword.java
+++ src/C03Inheritance/C0302SuperKeyword.java
@@ -1,6 +1,6 @@
 package C03Inheritance;
 
-public class C0302SuperKeyword {
+public class C0302SuperKeyword extends SuperParents{
     int a = 10;
     public static void main(String[] args) {
         C0302SuperKeyword c2 = new C0302SuperKeyword();
