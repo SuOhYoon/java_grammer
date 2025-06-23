c85fa2bbca41e194da6af731ce49d928f4ae11e8
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-23 09:48:22 +0900

    자동 커밋됨

--- src/C03Inheritance/C0302SuperKeyword.java
+++ src/C03Inheritance/C0302SuperKeyword.java
@@ -6,7 +6,7 @@ public class C0302SuperKeyword extends SuperParents{
 
     C0302SuperKeyword(){
 //        super() : 부모클래스의 생성자를 호출하는 메서드
-        super();
+        super(20);
     }
     public static void main(String[] args) {
         C0302SuperKeyword c2 = new C0302SuperKeyword();
