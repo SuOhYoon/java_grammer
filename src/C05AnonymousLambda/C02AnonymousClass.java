6c168712730b65cd63845ca414eb8af63ebb2ffb
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:07:59 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C02AnonymousClass.java
+++ src/C05AnonymousLambda/C02AnonymousClass.java
@@ -2,10 +2,12 @@ package C05AnonymousLambda;
 
 //익명 내부클래스
 public class C02AnonymousClass {
+    
 }
 
 abstract class AbstradtAnimal{
-    abstract void makeSound1(){
-        
+    abstract void makeSound1();
+    void makeSound2(){
+
     }
 }
