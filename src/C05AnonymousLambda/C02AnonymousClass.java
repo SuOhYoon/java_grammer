55053e326bcf5d50f0bff102277e0cbb6fd6eb5f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:36:13 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C02AnonymousClass.java
+++ src/C05AnonymousLambda/C02AnonymousClass.java
@@ -40,6 +40,9 @@ public class C02AnonymousClass {
         Animal3 a5 = (i1, i2, i3) -> {return i1 + ", " + i2 + ", " + i3;};
         Animal3 a6 = (i1, i2, i3) -> i1 + ", " + i2 + ", " + i3;
         System.out.println(a6.makeSound1("java", "python", "C++"));
+
+//        실습 : Animal4 인터페이스를 정의. makeSound메서드 선언
+        
     }
 }
 
