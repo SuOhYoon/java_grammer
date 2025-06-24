8b85437d379cdb450af4630c80684c0133c75467
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 14:45:25 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -1,4 +1,8 @@
 package C05AnonymousLambda;
 
 public class C04StreamAPi {
+    public static void main(String[] args) {
+        int[] arr = {20, 10, 4, 12};
+//        전통적인 방식의 데이터 접근방법 : 메모리 주소접근
+    }
 }
