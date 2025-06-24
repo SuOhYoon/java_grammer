bfea2e3eff9fe0adf055bf5a1fe77a3d30587335
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 15:42:24 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -70,5 +70,6 @@ public class C04StreamAPi {
 //        arr에서 홀수만 걸러서 해당 홀수의 제곱값을 담은 배열을 생성 후 출력
         int [] pow = Arrays.stream(odd).map(a->a*a).toArray();
         System.out.println(Arrays.toString(pow));
+        
     }
 }
