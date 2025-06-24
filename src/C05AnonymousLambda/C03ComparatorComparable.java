078341548c3af83467b328cb4c58e9b603f3a76f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 11:06:22 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C03ComparatorComparable.java
+++ src/C05AnonymousLambda/C03ComparatorComparable.java
@@ -65,5 +65,6 @@ class Student implements Comparable<Student>{
     @Override
     public int compareTo(Student o) {
         return this.getName().compareTo(o.getName());
+        return this.getAge()-o.getAge();
     }
 }
