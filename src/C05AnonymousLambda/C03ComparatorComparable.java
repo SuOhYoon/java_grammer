22aba9c59c89b9560e4c389e075188bdf122d4a3
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 10:30:28 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C03ComparatorComparable.java
+++ src/C05AnonymousLambda/C03ComparatorComparable.java
@@ -5,5 +5,8 @@ public class C03ComparatorComparable {
 //        자바에서는 비교를 위한 인터페이스가 대표적으로 2개가 제공
 //        Comparable인터페이스 : compareTo메서드 선언
 //        Comparator인터페이스 : compare메서드 선언
+        String a = "hello";
+        String b = "world";
+        
     }
 }
