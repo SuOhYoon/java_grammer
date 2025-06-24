4aa458a8defd07aa60822be120d279428d14a85f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 16:18:32 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -91,6 +91,6 @@ public class C04StreamAPi {
 
         String[] stArr2 = {"hello", "java", "world"};
 //        hellojavaworld
-        String concat =
+        String concat = Arrays.stream(st)
     }
 }
