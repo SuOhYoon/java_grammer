909569fed9f5ef9c19bec344fd4b5dbb452db7bd
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:49:24 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C03ComparatorComparable.java
+++ src/C05AnonymousLambda/C03ComparatorComparable.java
@@ -3,6 +3,6 @@ package C05AnonymousLambda;
 public class C03ComparatorComparable {
     public static void main(String[] args) {
 //        자바에서는 비교를 위한 인터페이스가 대표적으로 2개가 제공
-        
+//        Comparable인터페이스
     }
 }
