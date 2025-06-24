2d0536c8ed4f4e3f6a512edfb76ff67505e0c8bb
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 14:45:57 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -4,5 +4,8 @@ public class C04StreamAPi {
     public static void main(String[] args) {
         int[] arr = {20, 10, 4, 12};
 //        전통적인 방식의 데이터 접근방법 : 메모리 주소접근
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
