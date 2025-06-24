b162723f795ceef36568cab638a4fe52d965c04e
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:10:19 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C02AnonymousClass.java
+++ src/C05AnonymousLambda/C02AnonymousClass.java
@@ -3,7 +3,8 @@ package C05AnonymousLambda;
 //익명 내부클래스
 public class C02AnonymousClass {
     public static void main(String[] args) {
-//        AbstractAnimal을 상속한 클래스가 별도로 존재하지 않고, 
+//        AbstractAnimal을 상속한 클래스가 별도로 존재하지 않고,
+//        익명클래스가 만들어짐과 동시에 익명객체가 생성되고 있음.
         AbstractAnimal a1 = new AbstractAnimal() {
             @Override
             void makeSound1() {
