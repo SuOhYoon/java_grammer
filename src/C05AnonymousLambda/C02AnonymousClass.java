8a14ef10114c2c69e5903c77f29f519921451efb
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:15:12 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C02AnonymousClass.java
+++ src/C05AnonymousLambda/C02AnonymousClass.java
@@ -17,7 +17,12 @@ public class C02AnonymousClass {
         a1.makeSound2();
 
 //        구현체가 없는 추상클래스 또는 인터페이스는 익명객체로 생성 가능.
-        Animal1 a2 = new A
+        Animal1 a2 = new Animal1() {
+            @Override
+            public void makeSound1() {
+                
+            }
+        }
     }
 }
 
