aa0e101f102485c92e65ddd7f10694640d230a96
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-24 09:29:45 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C02AnonymousClass.java
+++ src/C05AnonymousLambda/C02AnonymousClass.java
@@ -37,7 +37,7 @@ public class C02AnonymousClass {
         Animal2 a4 = () -> System.out.println("hello world1");
 
 //        매개변수가 있는 익명객체의 람다함수 생성
-        Animal3 a5 = (a, b, c) -> null;
+        Animal3 a5 = (a, b, c) -> {null;
     }
 }
 
