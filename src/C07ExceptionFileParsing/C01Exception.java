b719747dec4f5083792a695c0ca03cce517d0f02
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 16:04:15 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -33,6 +33,8 @@ public class C01Exception {
     static boolean login(String password){
         if(password.equals("1234")){
             return true;
+        } else {
+            return false;
         }
     }
 }
