043969de9269f0b5bb93e807961bb4844c0f5b5f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 11:23:14 +0900

    자동 커밋됨

--- src/C06EtcClass/C01Math.java
+++ src/C06EtcClass/C01Math.java
@@ -3,6 +3,6 @@ package C06EtcClass;
 public class C01Math {
     public static void main(String[] args) {
 //        Math.random() : 임의의 수 반환. 0.0~1.0사이의 double형 반환
-        System.out.println();
+        System.out.println(Math.random());
     }
 }
