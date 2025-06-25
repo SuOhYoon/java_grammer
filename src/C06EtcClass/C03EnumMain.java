ac03feae02188a8ab9c16196a415784b7322e52b
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 14:13:59 +0900

    자동 커밋됨

--- src/C06EtcClass/C03EnumMain.java
+++ src/C06EtcClass/C03EnumMain.java
@@ -11,7 +11,7 @@ public class C03EnumMain {
 //        Student s2 = new Student("hong2", "firstGrade");
 //        Student s3 = new Student("hong3", "first_grade");
 
-//        static final을 통해 변수값 
+//        static final을 통해 변수값
 //        Student s1 = new Student("hong", c1);
 //        Student s2 = new Student("hong2", c2);
 //        Student s3 = new Student("hong3", c3);
@@ -31,9 +31,9 @@ class Student{
     }
 }
 
-class ClassGrade{
-    static final String c1 = "first_grade";
-    static final String c2 = "second_grade";
-    static final String c3 = "third_grade";
-
-}
+//class ClassGrade{
+//    static final String c1 = "first_grade";
+//    static final String c2 = "second_grade";
+//    static final String c3 = "third_grade";
+//
+//}
