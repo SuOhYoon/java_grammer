e78b15d5207536b679e5f3dc25b690ccf9dd409a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 14:32:32 +0900

    자동 커밋됨

--- src/C06EtcClass/C04GenericMain.java
+++ src/C06EtcClass/C04GenericMain.java
@@ -3,6 +3,6 @@ package C06EtcClass;
 public class C04GenericMain {
     public static void main(String[] args) {
         String[] stArr = {"java", "python", "C"};
-        
+        stchange(stArr, 0, 1);
     }
 }
