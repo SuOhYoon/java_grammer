18e1ab25bb4bb5491a667863ce5f92e40275d4ae
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 10:24:03 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -163,6 +163,5 @@ public class C04StreamAPi {
 
 //        방법4. (가장많이 사용) orElseThrow : 값이 있으면 있는 값 return, 없으면 지정된 예외(에러) 발생
         System.out.println(opt2_1.orElseThrow(()->new RuntimeException("값이 없습니다.")));
-        
     }
 }
