0e8b034efcdbf96c1321b82a9d3f2c4adc472da1
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 10:32:54 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -169,6 +169,6 @@ public class C04StreamAPi {
 //        optional 객체 예시
         List<Student> studentList = new ArrayList<>();
 
-        double age = studentList.stream().mapToInt(a->a.getAge()).average().getAsDouble()
+        double age = studentList.stream().mapToInt(a->a.getAge()).average().getAsDouble();
     }
 }
