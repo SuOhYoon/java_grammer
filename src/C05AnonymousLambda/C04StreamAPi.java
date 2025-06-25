c620df54575b99f8e7b8b4109b98436a9ebf2927
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 09:27:27 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -114,9 +114,8 @@ public class C04StreamAPi {
         studentList2.add(new Student("lee", 35));
         studentList2.add(new Student("park", 22));
 
-        Stream<Student> studentStream = studentList2.stream();
 //        1)모든 객체의 평균나이
-        
+
 
 
 
