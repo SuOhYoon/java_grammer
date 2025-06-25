171a6a8b00f6036c0949baf525c77daee512264d
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 14:09:18 +0900

    자동 커밋됨

--- src/C06EtcClass/C03EnumMain.java
+++ src/C06EtcClass/C03EnumMain.java
@@ -2,10 +2,10 @@ package C06EtcClass;
 
 public class C03EnumMain {
 
+    static final String c1 = "first_grade";
     static final String c1 = "firstGrade";
     static final String c1 = "firstGrade";
-    static final String c1 = "firstGrade";
-    
+
     public static void main(String[] args) {
 //        Student s1 = new Student("hong", "1학년");
 //        Student s2 = new Student("hong2", "firstGrade");
