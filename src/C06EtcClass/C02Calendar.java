c3c26acab3dfc0c10f293bd4677ce932617ef05b
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 12:15:41 +0900

    자동 커밋됨

--- src/C06EtcClass/C02Calendar.java
+++ src/C06EtcClass/C02Calendar.java
@@ -42,7 +42,7 @@ public class C02Calendar {
         System.out.println(localDateTime.get(ChronoField.CLOCK_HOUR_OF_DAY));
 
 //        .of : 임의로 특정시간정보객체를 만들어 내고 싶을때 사용
-        LocalDate birthDay = LocalDate.of(2000, 7, 12);
-        
+        LocalDate birthDay = LocalDate.of(2000, 11, 12);
+        System.out.println(birthDay);
     }
 }
