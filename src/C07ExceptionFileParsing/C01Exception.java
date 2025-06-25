b3fa933bc9f284d9025c4f6b35b49d358314a810
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 15:35:06 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -8,6 +8,7 @@ public class C01Exception {
         System.out.println("나눗셈 프로그램입니다.");
         Scanner sc = new Scanner(System.in);
         System.out.println("분자를 입력해주세요.");
-        int head = Integer.parseInt()
+        int head = Integer.parseInt(sc.nextLine());
+        
     }
 }
