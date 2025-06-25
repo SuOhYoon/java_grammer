85134a80df9df23fbbb6fcc43f418f486255e917
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 14:14:03 +0900

    자동 커밋됨

--- src/C06EtcClass/C03EnumMain.java
+++ src/C06EtcClass/C03EnumMain.java
@@ -16,9 +16,9 @@ public class C03EnumMain {
 //        Student s2 = new Student("hong2", c2);
 //        Student s3 = new Student("hong3", c3);
 
-        Student s1 = new Student("hong", ClassGrade.c1);
-        Student s2 = new Student("hong2", ClassGrade.c2);
-        Student s3 = new Student("hong3", ClassGrade.c3);
+//        Student s1 = new Student("hong", ClassGrade.c1);
+//        Student s2 = new Student("hong2", ClassGrade.c2);
+//        Student s3 = new Student("hong3", ClassGrade.c3);
 
     }
 }
