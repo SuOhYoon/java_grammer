cf1e08a3620e08e675369dbc6529e57e58fd8088
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 10:46:52 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -186,5 +186,6 @@ public class C04StreamAPi {
         studentList.add(new Student("lee", 20));
         studentList.add(new Student("park", 20));
         System.out.println("조회하고자 하는 학생의 index 번호를 입력해주세요.");
+        Scanner sc = new Scanner()
     }
 }
