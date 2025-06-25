6b1c2d8dbec1d7fd855718f78e73ef038f664a94
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 15:32:36 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -1,8 +1,11 @@
 package C07ExceptionFileParsing;
 
+import java.util.Scanner;
+
 public class C01Exception {
     public static void main(String[] args) {
 //        일반적인 예외처리 방식
         System.out.println("나눗셈 프로그램입니다.");
-    }
+        Scanner sc = new Scanner(System.in)
+;    }
 }
