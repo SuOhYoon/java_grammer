2767b6da2902785d49065b29d071d619a6ceabbb
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 16:46:35 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -34,7 +34,7 @@ public class C01Exception {
         System.out.println("로그인을 위한 비밀번호를 입력해주세요");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        login(input);
+//        login(input);
         System.out.println("환영합니다.");
 
         login2()
