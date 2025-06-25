d6e60ead87846dea96d1befcb362427e1e8e4c50
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 15:38:25 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -12,6 +12,8 @@ public class C01Exception {
             int head = Integer.parseInt(sc.nextLine());
             System.out.println("분모를 입력해주세요.");
             int tail = Integer.parseInt(sc.nextLine());
+            int result = head/tail;
+            System.out.println("두 수를 나눈 결과 값은 " + result);
 //            catch를 통해 예상되는 예외클래스를 분기처리한다.
         } catch {
 
