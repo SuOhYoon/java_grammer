aca3cf54bc723c529626555b47b8984465212bc2
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 11:43:24 +0900

    자동 커밋됨

--- src/C06EtcClass/C02Calendar.java
+++ src/C06EtcClass/C02Calendar.java
@@ -5,6 +5,46 @@ import java.util.Calendar;
 public class C02Calendar {
     public static void main(String[] args) {
 //        java.util패키지의 Calendar클래스
-        Calendar myCalendar = new
+        Calendar myCalendar = new Calendar() {
+            @Override
+            protected void computeTime() {
+                
+            }
+
+            @Override
+            protected void computeFields() {
+
+            }
+
+            @Override
+            public void add(int field, int amount) {
+
+            }
+
+            @Override
+            public void roll(int field, boolean up) {
+
+            }
+
+            @Override
+            public int getMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getMaximum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getGreatestMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getLeastMaximum(int field) {
+                return 0;
+            }
+        }
     }
 }
