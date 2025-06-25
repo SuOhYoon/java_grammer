8af28faec35a31558e9a9a924f83e96e00c4d081
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 15:07:52 +0900

    자동 커밋됨

--- src/C06EtcClass/C04GenericMain.java
+++ src/C06EtcClass/C04GenericMain.java
@@ -63,5 +63,11 @@ class Person{
         this.value = value;
     }
 
-    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
