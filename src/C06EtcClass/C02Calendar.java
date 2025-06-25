031e3c5ea18782126b909d25c189716c3c6469f4
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 12:07:06 +0900

    자동 커밋됨

--- src/C06EtcClass/C02Calendar.java
+++ src/C06EtcClass/C02Calendar.java
@@ -28,7 +28,7 @@ public class C02Calendar {
 
         LocalTime localTime = LocalTime.now();
         System.out.println(localTime.getHour());
-        System.out.println(localTime.getHour());
-        System.out.println(localTime.getHour());
+        System.out.println(localTime.getMinute());
+        System.out.println(localTime.getSecond());
     }
 }
