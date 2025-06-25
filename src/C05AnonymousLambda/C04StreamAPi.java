58cf643fb0629ba4f1d2f727036aedbb6e8065b1
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 10:37:52 +0900

    자동 커밋됨

--- src/C05AnonymousLambda/C04StreamAPi.java
+++ src/C05AnonymousLambda/C04StreamAPi.java
@@ -169,6 +169,7 @@ public class C04StreamAPi {
 //        optional 객체 예시1
         List<Student> studentList = new ArrayList<>();
 
+//        평균 구하기 방법 1   
         OptionalDouble age = studentList.stream().mapToInt(a->a.getAge()).average();
         if(age.isPresent()){
             System.out.println(age.getAsDouble());
