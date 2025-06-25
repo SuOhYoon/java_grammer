1d5b2e81340f668ed718ea64298ebebc666bef91
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 15:37:34 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -6,6 +6,8 @@ public class C01Exception {
     public static void main(String[] args) {
 //        일반적인 예외처리 방식
         try {
+
+        } catch {
             
         }
         System.out.println("나눗셈 프로그램입니다.");
