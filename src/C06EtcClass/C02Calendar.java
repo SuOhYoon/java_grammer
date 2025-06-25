25776c6c4e98df3a695a57847873d4f78fd206d4
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 11:43:50 +0900

    자동 커밋됨

--- src/C06EtcClass/C02Calendar.java
+++ src/C06EtcClass/C02Calendar.java
@@ -6,6 +6,6 @@ public class C02Calendar {
     public static void main(String[] args) {
 //        java.util패키지의 Calendar클래스
         Calendar myCalendar = Calendar.getInstance();
-        
+        System.out.println(myCalendar.getTime());
     }
 }
