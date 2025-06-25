6c300f84a852b1b2b87440cfd880d7b643378d8d
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-25 14:07:14 +0900

    자동 커밋됨

--- src/C06EtcClass/C03EnumMain.java
+++ src/C06EtcClass/C03EnumMain.java
@@ -3,6 +3,9 @@ package C06EtcClass;
 public class C03EnumMain {
     public static void main(String[] args) {
         Student s1 = new Student("hong", "1학년");
+        Student s1 = new Student("hong", "1학년");
+        Student s1 = new Student("hong", "1학년");
+        Student s1 = new Student("hong", "1학년");
 
     }
 }
