3b123cda124474f7a2ab7e51ef7a48b1ae2d3c56
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:37:56 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -26,7 +26,7 @@ public class C03JsonParsing {
         System.out.println(s1);
 
         Path filePath2 = Paths.get("src/C07ExceptionFileParsing/myjson2.json");
-        String st2 = Files.readString(filePath);
+        String st2 = Files.readString(filePath2);
         List<Student> studentList = (List<Student>) o1.readValue(st2, Map.class);
 //        사용법2. 트리구조의 JsonNode로 변환 : JsonNode 객체명 = o1.readTree(문자열)
 //        복잡한 json자료구조일 경우에 트리구조의 JsonNode 사용
