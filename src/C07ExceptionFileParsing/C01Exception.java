c5bd643632d2f362f2ad6426f7eeac6152f4e483
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 09:27:38 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -95,7 +95,6 @@ public class C01Exception {
 
     static String fileRead(String path){
         Path filePath = Paths.get(path);
-        String text = filePath.toString();
-        return text;
+        return filePath.toString();
     }
 }
