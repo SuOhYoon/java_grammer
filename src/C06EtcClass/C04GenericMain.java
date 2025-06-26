ed52da09a70428cef85eae7da4cb0dc5f132c4fe
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 09:11:09 +0900

    자동 커밋됨

--- src/C06EtcClass/C04GenericMain.java
+++ src/C06EtcClass/C04GenericMain.java
@@ -1,47 +1,57 @@
 package C06EtcClass;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class C04GenericMain {
     public static void main(String[] args) {
-        String[] stArr = {"java", "python", "C"};
-        stchange(stArr, 0, 1);
-        System.out.println(Arrays.toString(stArr));
+        String[] strArr = {"java", "python", "c++"};
+        strArrChange(strArr, 0, 1);
+        System.out.println(Arrays.toString(strArr));
 
-        Integer [] intArr = {10, 20, 30};
-        intchange(intArr, 0, 1);
+        Integer[] intArr = {3, 5, 10};
+        intArrChange(intArr, 0, 1);
         System.out.println(Arrays.toString(intArr));
 
-        allchange(stArr, 0, 1);
-        allchange(intArr, 0, 1);
+        allChange(strArr, 1, 2);
+        System.out.println(Arrays.toString(strArr));
 
-//        일반객체 생성시
+        allChange(intArr, 1, 2);
+        System.out.println(Arrays.toString(intArr));
+
+//        제네릭 사용 예시
+        List<String> myList = new ArrayList<>();
+        Stream<String> myStream;
+
+//        일반 객체 생성시
         Person p1 = new Person("hongildong");
-        GenericPerson<Integer>p2 = new GenericPerson<Integer>(10);
-        GenericPerson<String>p3 = new GenericPerson<String>("hong");
+        GenericPerson<String> p2 = new GenericPerson<>("hong");
+        GenericPerson<Integer> p3 = new GenericPerson<>(1);
     }
 
-//    제네릭메서드는 반환타입 왼족에 <T>라고 선언
-//    이때 T에는 참조형 변수인 객체타입만을 허용
-    static<T> void allchange(T[] arr, int a, int b){
-        T temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-    static void stchange(String[] arr, int a, int b){
-        String temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+    private static <T> void allChange(T[] arr, int idx1, int idx2) {
+        T tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
     }
 
-    static void intchange(Integer[] arr, int a, int b){
-        Integer temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+    private static void strArrChange(String[] strArr, int idx1, int idx2) {
+        String tmp = strArr[idx1];
+        strArr[idx1] = strArr[idx2];
+        strArr[idx2] = tmp;
     }
+
+    private static void intArrChange(Integer[] intArr, int idx1, int idx2) {
+        Integer tmp = intArr[idx1];
+        intArr[idx1] = intArr[idx2];
+        intArr[idx2] = tmp;
+    }
+
 }
 
-class GenericPerson<T>{
+class GenericPerson<T> {
     private T value;
 
     public GenericPerson(T value) {
@@ -56,7 +66,8 @@ class GenericPerson<T>{
         this.value = value;
     }
 }
-class Person{
+
+class Person {
     private String value;
 
     public Person(String value) {
