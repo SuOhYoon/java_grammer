e1ed3ce03137d56478f56d4e12838bc321a4a56a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:04:19 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberController.java
+++ src/C07ExceptionFileParsing/MemberException/MemberController.java
@@ -11,7 +11,8 @@ public class MemberController {
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.println("입금하시려면 1번, 출금하시려면 2번");
-            
+
         }
+        
     }
 }
