f4f192b3dcd7ea9dfa4b81c2fc2d7d23b4b78926
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:35:02 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C02File.java
+++ src/C07ExceptionFileParsing/C02File.java
@@ -1,7 +1,11 @@
 package C07ExceptionFileParsing;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class C02File {
     public static void main(String[] args) {
-        
+//        콘솔창에 입출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
