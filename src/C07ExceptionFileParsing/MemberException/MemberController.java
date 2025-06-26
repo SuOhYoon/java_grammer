64395340c5c22415a1159a0300096188edc1c63c
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:02:49 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberController.java
+++ src/C07ExceptionFileParsing/MemberException/MemberController.java
@@ -4,8 +4,11 @@ import C04Interface.BankService.BankKakaoService;
 import C04Interface.BankService.BankService;
 import C04Interface.BankService.BankTossService;
 
+import java.util.Scanner;
+
 public class MemberController {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         while(true){
             System.out.println("입금하시려면 1번, 출금하시려면 2번");
             String serviceNumber = sc.nextLine();
