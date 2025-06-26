41a327b7cc8f2e4e58beaf9ebc6780aa325e82f8
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:02:35 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberController.java
+++ src/C07ExceptionFileParsing/MemberException/MemberController.java
@@ -1,7 +1,35 @@
 package C07ExceptionFileParsing.MemberException;
 
+import C04Interface.BankService.BankKakaoService;
+import C04Interface.BankService.BankService;
+import C04Interface.BankService.BankTossService;
+
 public class MemberController {
     public static void main(String[] args) {
-        
+        while(true){
+            System.out.println("입금하시려면 1번, 출금하시려면 2번");
+            String serviceNumber = sc.nextLine();
+            System.out.println("사용하는 은행을 선택해주세요. 1.카카오페이 2.토스페이");
+            String bank = sc.nextLine();
+            BankService bs;
+
+            if(bank.equals("1")){
+                bs = new BankKakaoService();
+            } else {
+                bs = new BankTossService();
+            }
+
+            if(serviceNumber.equals("1")){
+                System.out.println("입금하실 금액을 입력해주세요");
+                long money = Long.parseLong(sc.nextLine());
+                bs.deposit(money, ba);
+                System.out.println("입금이 완료되었습니다.");
+            } else {
+                System.out.println("출금하실 금액을 입력해주세요");
+                long money  = Long.parseLong(sc.nextLine());
+                bs.withdraw(money, ba);
+                System.out.println("출금이 완료되었습니다.");
+            }
+        }
     }
 }
