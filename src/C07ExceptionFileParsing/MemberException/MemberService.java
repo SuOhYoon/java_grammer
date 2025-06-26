710b336758586fade0e84d2d651ef70e709cf7d3
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:01:43 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberService.java
+++ src/C07ExceptionFileParsing/MemberException/MemberService.java
@@ -2,7 +2,7 @@ package C07ExceptionFileParsing.MemberException;
 
 public class MemberService {
     public void register(String name, String email, String password){
-//        DB에
+//        DB에 이메일이 중복일 경우 예외 발생
 
     }
 
