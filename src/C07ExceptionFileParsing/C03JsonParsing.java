69dc80eb96e537948da71c91dfef31c98c34d4d4
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:46:17 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -44,7 +44,7 @@ public class C03JsonParsing {
         List<Student> studentList = new ArrayList<>();
         for(JsonNode j : jsonNodes2){
             System.out.println(j);
-            Student s1 = o1.readValue();
+            Student s1 = o1.readValue(j.toString());
             studentList.add(s1);
         }
 
