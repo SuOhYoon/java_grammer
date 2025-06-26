12f88dcddc693c46d7afbdc7e252ac89677fb2e7
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 09:29:39 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -99,6 +99,7 @@ public class C01Exception {
     static String fileRead(String path){
         Path filePath = Paths.get(path);
 //        checked Exception의 경우 예외처리가 강제
+//        방법1.
         try {
             String text = Files.readString(filePath);
         } catch (IOException e) {
