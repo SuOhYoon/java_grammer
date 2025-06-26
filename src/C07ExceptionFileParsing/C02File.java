ce85c8338bb9d2236c0473297b4942160891aea0
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:54:07 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C02File.java
+++ src/C07ExceptionFileParsing/C02File.java
@@ -31,5 +31,6 @@ public class C02File {
         System.out.println(st1);
 //        readAllLines : 문자열을 라인별로 split하여 list형태로 저장
         List<String> st2 = Files.readAllLines(filePath);
+        for
     }
 }
