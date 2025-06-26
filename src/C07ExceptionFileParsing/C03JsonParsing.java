c51ec029a085f7f7e4d994d96e41df5d6624a25c
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:29:27 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -27,6 +27,7 @@ public class C03JsonParsing {
         String st2 = Files.readString(filePath);
         List<Student> studentList = o1.readValue(st2, Map.class);
 //        사용법2. 트리구조의 JsonNode로 변환 : JsonNode 객체명 = o1.readTree(문자열)
+//        복잡한 json자료구조일 경우에 트리구조의 JsonNode 사용
 
     }
 }
