2bbec8fea47548ea3f872460db727ec2036f0b5a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 10:10:29 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberRepository.java
+++ src/C07ExceptionFileParsing/MemberException/MemberRepository.java
@@ -1,6 +1,11 @@
 package C07ExceptionFileParsing.MemberException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // 저장소역할을 하는 계층
 // DB에 CRUD를 수행하는 계층
 public class MemberRepository {
+    static List<Member> memberList = new ArrayList<>();
+    public void register
 }
