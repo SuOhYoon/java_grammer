f0037854e62bc1ecb5e0741b554586f0d24e74e4
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 15:48:16 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -1,8 +1,11 @@
 package C07ExceptionFileParsing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class C03JsonParsing {
     public static void main(String[] args) {
-        
 //        ObjectMapper : json 직렬화 역직렬화를 위한 라이브러리
+        ObjectMapper o1 = new ObjectMapper();
+//        사용법 : 클래스명 객체명 = o1.readValue(문자열, 클래스명);
     }
 }
