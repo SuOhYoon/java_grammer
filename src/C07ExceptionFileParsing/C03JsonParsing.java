4b1966e3ed5d14513c98ddaf2c9a488b8423ec68
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:40:17 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -42,6 +42,11 @@ public class C03JsonParsing {
 //        실습 : myJson2를 가지고 List<Student> 객체 형식으로 변환
         JsonNode jsonNodes2 = o1.readTree(st2);
         Map<String, String> mymap3 = new HashMap<>();
+        myMap2.put("id", jsonNodes.get("id").asText());
+        myMap2.put("name", jsonNodes.get("name").asText());
+        myMap2.put("classNumber", jsonNodes.get("classNumber").asText());
+        myMap2.put("city", jsonNodes.get("city").asText());
+        
         List<Student> students = new ArrayList<>();
         students.add(new Student())
 
