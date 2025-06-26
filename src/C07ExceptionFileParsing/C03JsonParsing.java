db6f67903ccda455df87341d8249f4b11eee0b7f
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:41:31 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -40,6 +40,7 @@ public class C03JsonParsing {
         System.out.println(myMap2);
 
 //        실습 : myJson2를 가지고 List<Student> 객체 형식으로 변환
+        
 //        JsonNode jsonNodes2 = o1.readTree(st2);
 //        Map<String, String> myMap3 = new HashMap<>();
 //        myMap3.put("id", jsonNodes.get("id").asText());
