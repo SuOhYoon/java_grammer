7a6b3170177bf9022f74161f54acc721e97473e9
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 12:07:29 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/Member.java
+++ src/C07ExceptionFileParsing/MemberException/Member.java
@@ -6,5 +6,35 @@ public class Member {
     String email;
     String password;
 
-    
+    public static long getId() {
+        return id;
+    }
+
+    public static void setId(long id) {
+        Member.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
