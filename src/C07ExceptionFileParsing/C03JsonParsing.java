e3814b91a587c81d655a8119b7a214c721af5f89
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 16:05:23 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C03JsonParsing.java
+++ src/C07ExceptionFileParsing/C03JsonParsing.java
@@ -13,9 +13,9 @@ public class C03JsonParsing {
 //        ObjectMapper : json 직렬화 역직렬화를 위한 라이브러리
         ObjectMapper o1 = new ObjectMapper();
 //        사용법 : 클래스명 객체명 = o1.readValue(문자열, 클래스명);
-        Path filePath = Paths.get("src/C07ExceptionFileParsing/test.txt");
+        Path filePath = Paths.get("src/C07ExceptionFileParsing/myjson1.json");
         String st1 = Files.readString(filePath);
         Map<String, String> myMap = o1.readValue(st1);
-        
+
     }
 }
