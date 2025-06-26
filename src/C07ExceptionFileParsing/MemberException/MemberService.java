8e739f4a0836898f171054359b45d0ca6ab10caf
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 12:24:47 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberService.java
+++ src/C07ExceptionFileParsing/MemberException/MemberService.java
@@ -1,7 +1,7 @@
 package C07ExceptionFileParsing.MemberException;
 
 public class MemberService {
-    public void register(){
-        
+    public void register(String name, String email, String password){
+
     }
 }
