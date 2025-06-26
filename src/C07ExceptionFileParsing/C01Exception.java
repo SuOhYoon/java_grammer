f86fd2f3b70019a0ac1a523924ae419c9b8f8d13
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 09:15:45 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -58,7 +58,7 @@ public class C01Exception {
     static boolean login(String password) throws IllegalArgumentException {
         if (password.equals("1234")) {
             return true;
-        } else {
+        } else if(password == null){
 //            예외를 강제 발생시킴으로서 이시점에 해당 메서드 강제 종료
 //            예외는 기본적으로 해당메서드를 호출한 쪽으로 전파
             throw new IllegalArgumentException("비밀번호를 잘못입력하셨습니다.");
