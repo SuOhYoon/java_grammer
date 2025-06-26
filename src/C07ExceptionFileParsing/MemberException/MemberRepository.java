5e5af709327bdddebd91a47eaf790146f56d2f06
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-26 14:32:20 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/MemberException/MemberRepository.java
+++ src/C07ExceptionFileParsing/MemberException/MemberRepository.java
@@ -2,20 +2,27 @@ package C07ExceptionFileParsing.MemberException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-// 저장소역할을 하는 계층
-// DB에 CRUD를 수행하는 계층
+//저장소역할을 하는 계층
+//DB에 CRUD를 수행하는 계층
 public class MemberRepository {
-    static List<Member> memberList = new ArrayList<>();
-
-    Member member = new Member();
-    public void register(String name, String email, String password){
-        member.setName(name);
-        member.setEmail(email);
-        member.setPassword(password);
+    private static List<Member> memberList = new ArrayList<>();
+    //    회원가입
+    public void register(Member member){
+        memberList.add(member);
     }
 
-    public Member findById(long id){
-        return new Member().getName()
+    public Optional<Member> findByEmail(String email){
+        return memberList.stream().filter(m->m.getEmail().equals(email)).findFirst();
+    }
+    //    회원상세조회
+    public Optional<Member> findById(Long id){
+        return memberList.stream().filter(m->m.getId()==id).findFirst();
+    }
+
+    //    회원목록조회
+    public List<Member> findAll(){
+        return memberList;
     }
 }
