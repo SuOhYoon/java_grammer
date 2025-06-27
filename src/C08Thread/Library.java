1d0e03eddf63c5743d2a08e097583db5267e11b8
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 10:20:55 +0900

    자동 커밋됨

--- src/C08Thread/Library.java
+++ src/C08Thread/Library.java
@@ -4,7 +4,7 @@ public class Library {
     private static int bookCount = 100;
 
     public static void borrow(){
-        
+        if(bookCount>0)
     }
 
     public static int getBookCount() {
