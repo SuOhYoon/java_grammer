80e8589102969a782a562acd844d46ca8844050a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 11:26:42 +0900

    자동 커밋됨

--- src/C08Thread/Library.java
+++ src/C08Thread/Library.java
@@ -3,6 +3,7 @@ package C08Thread;
 public class Library {
     private static int bookCount = 100;
 
+//    synchronized : 메서드 내에서 1개의 스레드만 실행되도록 강제.
     public synchronized static void borrow(){
         if(bookCount>0){
             try {
