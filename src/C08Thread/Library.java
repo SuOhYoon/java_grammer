e06cd60283a872c47535ea4d6de920b93400a72a
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 12:04:31 +0900

    자동 커밋됨

--- src/C08Thread/Library.java
+++ src/C08Thread/Library.java
@@ -2,7 +2,7 @@ package C08Thread;
 
 public class Library {
     private static int bookCount = 100;
-    
+
 //    synchronized : 메서드 내에서 1개의 스레드만 실행되도록 강제.
 //    rdb에 재고관리를 할경우에는 syncronized를 통한다 하더라도, 쿼리와 commit실행시점에 차이로 인해 동시성이슈 발생.
 //    public synchronized static void borrow(){
