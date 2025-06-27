25a122754b0af9ee99fec07006f2ee99684cf6ba
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 09:19:04 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C04HttpJsonParsing.java
+++ src/C07ExceptionFileParsing/C04HttpJsonParsing.java
@@ -3,6 +3,7 @@ package C07ExceptionFileParsing;
 public class C04HttpJsonParsing {
     public static void main(String[] args) {
 //        외부데이터(api) 요청 : 서버간의 통신
-//        
+//        RestTemplate, RestClient : 스프링에서 가장 많이 사용하는 라이브러리
+        
     }
 }
