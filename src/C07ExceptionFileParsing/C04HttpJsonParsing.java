ddb2f2afd70e2c4cc42aea4cdb1d78d7e145506b
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 09:24:41 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C04HttpJsonParsing.java
+++ src/C07ExceptionFileParsing/C04HttpJsonParsing.java
@@ -1,7 +1,9 @@
 package C07ExceptionFileParsing;
 
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class C04HttpJsonParsing {
     public static void main(String[] args) {
@@ -9,6 +11,10 @@ public class C04HttpJsonParsing {
 //        RestTemplate, RestClient : 스프링에서 가장 많이 사용하는 라이브러리
 //        HttpClient는 자바의 내장클래스로서 별도의 역직렬화, 직렬화 작업이 필요
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri()
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
