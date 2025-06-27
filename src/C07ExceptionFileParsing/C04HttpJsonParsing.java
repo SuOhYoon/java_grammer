955a11fa3042cd7c4d409ddc31c944dbdc012fb3
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 09:46:59 +0900

    자동 커밋됨

--- src/C07ExceptionFileParsing/C04HttpJsonParsing.java
+++ src/C07ExceptionFileParsing/C04HttpJsonParsing.java
@@ -69,5 +69,13 @@ class Post{
         return body;
     }
 
-
+    @Override
+    public String toString() {
+        return "Post{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
