9f2b442dc29135fa38a5fc8ea20c3af40d3da942
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 10:23:39 +0900

    자동 커밋됨

--- src/C08Thread/ThreadMain.java
+++ src/C08Thread/ThreadMain.java
@@ -2,6 +2,9 @@ package C08Thread;
 
 public class ThreadMain {
     public static void main(String[] args) {
-        
+//        단일스레드 실행
+        for(int i=0;i<1000;i++){
+            Library.borrow();
+        }
     }
 }
