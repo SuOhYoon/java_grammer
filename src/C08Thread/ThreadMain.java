40f13bf0eeda0340762da2f3552cce925ca6cdcf
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-06-27 10:43:09 +0900

    자동 커밋됨

--- src/C08Thread/ThreadMain.java
+++ src/C08Thread/ThreadMain.java
@@ -10,21 +10,21 @@ public class ThreadMain {
 
 //        멀티스레드 생성
 //        방법1. 스레드 클래스 상속방식
-        Thread t1 = new MyThread();
-        Thread t2 = new MyThread();
-        Thread t3 = new MyThread();
-        Thread t4 = new MyThread();
-//        Thread클래스안에 start메서드 내장되어있고, start메서드는 run메서드 호출하면서 스레드 생성.
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        System.out.println("hello world");
+//        Thread t1 = new MyThread();
+//        Thread t2 = new MyThread();
+//        Thread t3 = new MyThread();
+//        Thread t4 = new MyThread();
+////        Thread클래스안에 start메서드 내장되어있고, start메서드는 run메서드 호출하면서 스레드 생성.
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//        System.out.println("hello world");
 //        방법2. Runnalbe을 직접 구현한 객체를 Thread클래스 생성자에 주입하는 방식
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                
+
             }
         })
     }
